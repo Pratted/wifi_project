ce43aa427942f2bc03e3fd6987c446ac342ded4d
Eric Pratt <eric@Erics-MacBook-Pro.local>
2019-01-27 18:56:48 -0500

    Added boilerplate code for client

---
+++ client/app/src/main/java/com/example/eric/wishare/WiMessagingService.java
@@ -0,0 +1,56 @@
+package com.example.eric.wishare;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class WiMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "WiMessagingService";
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final Integer SERVER_PORT = 5000;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            // TODO: handle the data message
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+    // send a message to our server
+    public boolean sendMessage(String json){
+        try {
+            Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+
+            OutputStream os = socket.getOutputStream();
+            os.write(json.getBytes());
+            return true;
+
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public void onNewToken(String token){
+        Log.d(TAG, "The new token is: " + token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
