ec212501e87fbdfa1bc680b9ec19ec35baa3037b
Jacob Claude Fullmer <jcf5319@frege.cs.hbg.psu.edu>
2019-02-01 14:26:15 -0500

    Added dialogs for adding network

--- client/app/src/main/java/com/example/eric/wishare/MainActivity.java
+++ client/app/src/main/java/com/example/eric/wishare/MainActivity.java
@@ -3,22 +3,67 @@ package com.example.eric.wishare;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "MainActivity";
+
+    private MaterialDialog.ListCallback onNetWorkSelect() {
+        return new MaterialDialog.ListCallback() {
+            @Override
+            public void onSelection(MaterialDialog dialog, View itemView, int position, CharSequence text) {
+                new MaterialDialog.Builder(MainActivity.this)
+                        .title("Enter Password")
+                        .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)
+                        .input("Password", "", false, new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(@NonNull MaterialDialog dialog, CharSequence input) {
+                                Toast.makeText(MainActivity.this, "You entered " + input, Toast.LENGTH_LONG).show();
+                            }
+                        }).show();
+            }
+        };
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.btn_add_network).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> networks = new ArrayList<>();
+                networks.add("Hyuntaes crib");
+                networks.add("Eric Home");
+                networks.add("Foo");
+                networks.add("Bar");
+                networks.add("Club");
+
+                new MaterialDialog.Builder(MainActivity.this)
+                    .title("Select Network")
+                    .items(networks)
+                    .itemsCallback(onNetWorkSelect())
+                    .negativeText("Cancel")
+                    .show();
+
+
+
+            }
+        });
 
 
         FirebaseInstanceId.getInstance().getInstanceId()
@@ -36,7 +81,12 @@ public class MainActivity extends AppCompatActivity {
                 // Log and toast
                 Log.d(TAG, "The token is: " + token);
                 Toast.makeText(MainActivity.this, "The token is: " + token , Toast.LENGTH_SHORT).show();
+
             }
         });
     }
+
+
+
+
 }
