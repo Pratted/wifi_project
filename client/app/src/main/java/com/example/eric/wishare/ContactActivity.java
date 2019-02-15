62574a65901a0441c5263fa7fa528d4c6b3b3266
jzv5153 <josephvu519@gmail.com>
2019-02-15 12:36:07 -0500

    Contact name and phone number display (work in progress)

--- client/app/src/main/java/com/example/eric/wishare/ContactActivity.java
+++ client/app/src/main/java/com/example/eric/wishare/ContactActivity.java
@@ -1,7 +1,18 @@
 package com.example.eric.wishare;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,5 +20,18 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        onContactSelected();
+
+
+    }
+    private WiContactListDialog.OnContactSelectedListener onContactSelected(){
+        return new WiContactListDialog.OnContactSelectedListener() {
+            @Override
+            public void onContactSelected(WiContact contact) {
+                ((TextView)findViewById(R.id.tv_contact_name)).setText(contact.getName());
+                ((TextView)findViewById(R.id.tv_contact_number)).setText(contact.getPhone());
+                Toast.makeText(ContactActivity.this, "Hello", Toast.LENGTH_LONG).show();
+            }
+        };
     }
 }
--- client/app/src/main/java/com/example/eric/wishare/WiContact.java
+++ client/app/src/main/java/com/example/eric/wishare/WiContact.java
@@ -8,4 +8,20 @@ public class WiContact {
         this.name = name;
         this.phone = phone;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
