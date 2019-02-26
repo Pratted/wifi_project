d51f687c5fd4f00a00ebd8cf081a4baa882f533f
jacobfullmer <jcf5319@psu.edu>
2019-02-26 00:22:09 -0500

    UI work

--- client/app/src/main/java/com/example/eric/wishare/ContactActivity.java
+++ client/app/src/main/java/com/example/eric/wishare/ContactActivity.java
@@ -1,6 +1,7 @@
 package com.example.eric.wishare;
 
 import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -24,6 +25,8 @@ public class ContactActivity extends AppCompatActivity {
     private Button btnRevokeAccess;
     private Button btnAddContactToNetwork;
 
+    private ArrayList<WifiConfiguration> mNetworks;
+
 
     private WiAddContactToNetworkDialog mAddToNetwork;
     private WiRevokeAccessDialog mRevokeAccessDialog;
@@ -31,6 +34,14 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        mNetworks = WiNetworkManager.getConfiguredNetworks(this);
+
+        ArrayList<String> networkList = new ArrayList<>();
+
+        for (WifiConfiguration configuration : mNetworks) {
+            networkList.add(configuration.SSID);
+        }
 
         WiContact contact = getIntent().getExtras().getParcelable("contact");
 
@@ -56,23 +67,23 @@ public class ContactActivity extends AppCompatActivity {
 
 
         //Using hard coded placeholders
-        ((TextView) layout.findViewById(R.id.tv_network_name)).setText("Port Sarim");
+        ((TextView) layout.findViewById(R.id.tv_network_name)).setText(mNetworks.get(0).SSID);
         ((TextView) layout.findViewById(R.id.tv_connection_status)).setText("Invitation sent");
         mScrollView.addView(layout);
         layout = (LinearLayout) inflater.inflate(R.layout.layout_permitted_network_list_item, null);
-        ((TextView) layout.findViewById(R.id.tv_network_name)).setText("Varrock");
+        //((TextView) layout.findViewById(R.id.tv_network_name)).setText(mNetworks.get(1).SSID);
         ((TextView) layout.findViewById(R.id.tv_connection_status)).setText("Connected");
         mScrollView.addView(layout);
         layout = (LinearLayout) inflater.inflate(R.layout.layout_permitted_network_list_item, null);
-        ((TextView) layout.findViewById(R.id.tv_network_name)).setText("Lumbridge");
+        //((TextView) layout.findViewById(R.id.tv_network_name)).setText(mNetworks.get(2).SSID);
         ((TextView) layout.findViewById(R.id.tv_connection_status)).setText("Data limit reached");
         mScrollView.addView(layout);
         layout = (LinearLayout) inflater.inflate(R.layout.layout_permitted_network_list_item, null);
-        ((TextView) layout.findViewById(R.id.tv_network_name)).setText("Edgeville");
+        //((TextView) layout.findViewById(R.id.tv_network_name)).setText(mNetworks.get(3).SSID);
         ((TextView) layout.findViewById(R.id.tv_connection_status)).setText("Not connected");
         mScrollView.addView(layout);
         layout = (LinearLayout) inflater.inflate(R.layout.layout_permitted_network_list_item, null);
-        ((TextView) layout.findViewById(R.id.tv_network_name)).setText("Falador");
+        //((TextView) layout.findViewById(R.id.tv_network_name)).setText(mNetworks.get(4).SSID);
         ((TextView) layout.findViewById(R.id.tv_connection_status)).setText("Invitation expired");
         mScrollView.addView(layout);
 
