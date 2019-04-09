7e0669ccda6f27e69686f54ae14182515ce2f0e1
Joseph Vu <33297434+josephvu519@users.noreply.github.com>
2019-04-09 14:08:29 -0400

    Linked revoke access buttons

--- client/app/src/main/java/com/example/eric/wishare/model/messaging/WiRevokeAccessDataMessage.java
+++ client/app/src/main/java/com/example/eric/wishare/model/messaging/WiRevokeAccessDataMessage.java
@@ -1,4 +1,20 @@
 package com.example.eric.wishare.model.messaging;
 
-public class WiRevokeAccessDataMessage {
+import com.example.eric.wishare.model.WiConfiguration;
+import com.example.eric.wishare.model.WiContact;
+
+import org.json.JSONObject;
+
+public class WiRevokeAccessDataMessage extends WiDataMessage{
+
+    public WiRevokeAccessDataMessage(WiConfiguration config, String recipient){
+        super(MSG_CREDENTIALS);
+        deepCopy(config.toJSONObject());
+        addRecipient(recipient);
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+
+    }
 }
