3b457a4ca4a23bb8b909e5f48cd6bc6507537a12
Eric Pratt <eric@Erics-MacBook-Pro.local>
2019-04-30 19:09:17 -0400

    Moved Pager Adapter to its own file

--- client/app/src/main/java/com/example/eric/wishare/view/WiTabbedScrollView.java
+++ client/app/src/main/java/com/example/eric/wishare/view/WiTabbedScrollView.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import com.example.eric.wishare.R;
 import com.example.eric.wishare.WiContactList;
+import com.example.eric.wishare.WiPagerAdapter;
 import com.example.eric.wishare.WiSQLiteDatabase;
 import com.example.eric.wishare.model.WiConfiguration;
 import com.example.eric.wishare.model.WiContact;
@@ -30,7 +31,6 @@ public class WiTabbedScrollView extends LinearLayout {
     private WiPagerAdapter mPagerAdapter;
     private WiContactList mContactList;
     private WeakReference<Context> mContext;
-
 
     private WiPermittedContactsView mPermittedContactsView;
     private WiInvitableContactsView mInvitableContactsView;
@@ -130,99 +130,5 @@ public class WiTabbedScrollView extends LinearLayout {
         mPermittedContactsView.filter(searchString);
         mInvitableContactsView.filter(searchString);
     }
-
-    private class WiPagerAdapter extends PagerAdapter {
-        private ArrayList<View> pages = new ArrayList<>();
-
-        public int getItemPosition(Object object) {
-            return POSITION_NONE;
-        }
-
-        //-----------------------------------------------------------------------------
-        // Used by ViewPager.  Called when ViewPager needs a page to display; it is our job
-        // to add the page to the container, which is normally the ViewPager itself.  Since
-        // all our pages are persistent, we simply retrieve it from our "views" ArrayList.
-        @Override
-        public Object instantiateItem (ViewGroup container, int position)
-        {
-            View v = pages.get (position);
-
-            final TextView tv2 = new TextView(getContext());
-            tv2.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
-            tv2.setText("Haha wtf?");
-
-            tv2.setHeight(100);
-            //((LinearLayout) ((ScrollView) temp.getChildAt(0)).getChildAt(0)).addView(tv2);
-
-            container.addView (v);
-            return v;
-        }
-
-        //-----------------------------------------------------------------------------
-        // Used by ViewPager.  Called when ViewPager no longer needs a page to display; it
-        // is our job to remove the page from the container, which is normally the
-        // ViewPager itself.  Since all our pages are persistent, we do nothing to the
-        // contents of our "views" ArrayList.
-        @Override
-        public void destroyItem (ViewGroup container, int position, Object object)
-        {
-            container.removeView (pages.get (position));
-        }
-
-        //-----------------------------------------------------------------------------
-        // Used by ViewPager; can be used by app as well.
-        // Returns the total number of pages that the ViewPage can display.  This must
-        // never be 0.
-        @Override
-        public int getCount ()
-        {
-            return pages.size();
-        }
-
-
-        //-----------------------------------------------------------------------------
-        // Used by ViewPager.
-        @Override
-        public boolean isViewFromObject (View view, Object object) {
-            return view == object;
-        }
-
-        //-----------------------------------------------------------------------------
-        // Add "view" to right end of "views".
-        // Returns the position of the new view.
-        // The app should call this to add pages; not used by ViewPager.
-        public int addView (View v) {
-            return addView (v, pages.size());
-        }
-
-        //-----------------------------------------------------------------------------
-        // Add "view" at "position" to "views".
-        // Returns position of new view.
-        // The app should call this to add pages; not used by ViewPager.
-        public int addView (View v, int position) {
-            pages.add (position, v);
-            return position;
-        }
-
-        //-----------------------------------------------------------------------------
-        // Removes the "view" at "position" from "views".
-        // Retuns position of removed view.
-        // The app should call this to remove pages; not used by ViewPager.
-        public int removeView (ViewPager pager, int position) {
-            // ViewPager doesn't have a delete method; the closest is to set the adapter
-            // again.  When doing so, it deletes all its views.  Then we can delete the view
-            // from from the adapter and finally set the adapter to the pager again.  Note
-            // that we set the adapter to null before removing the view from "views" - that's
-            // because while ViewPager deletes all its views, it will call destroyItem which
-            // will in turn cause a null pointer ref.
-
-            pager.setAdapter (null);
-            pages.remove (position);
-            pager.setAdapter (this);
-
-            return position;
-        }
-    }
-
 }
 
